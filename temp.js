3beaffd6562a48b54c0786a73c9204021cef0269
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-09-20 13:55:06 +0530

    done

--- temp.js
+++ temp.js
@@ -1,42 +1,7 @@
-let developers_array = [
-    {
-      firstName: "John",
-      lastName: "Doe",
-      dept: "FE",
-      commits: 10
-    },
-    {
-      firstName: "Jane",
-      lastName: "Doe",
-      dept: "BE",
-      commits: 15
-    },
-    {
-      firstName: "James",
-      lastName: "bond",
-      dept: "BE",
-      commits: 8
-    }]; 
+num = 123
 
-// expected result : { BE: ["Jane", "James"], FE: ["John"]} 
-// let result = developers_array.reduce((acc, current) => {
-//     current.dept === "BE" ?
-//         acc.BE.push(current.firstName) :
-//         acc.FE.push(current.firstName);
-    
-//     return acc;
-        
+s = num.toString()
 
-res = developers_array.reduce((acc, item) => {
-    if(item.dept === "FE"){
-        acc.FE.push(item.firstName)
-    }else{
-        acc.BE.push(item.firstName)
-    }
-    return acc
-}, {
-    BE : [],
-    FE : []
-})
+console.log(s.parseInt());
 
-console.log(res)
+
